ee9ec58540b7ee1643fcbee6721842d3ed31c5fa
WMage <bsebi88@gmail.com>
2022-11-13 23:46:35 +0100

    fix-done:tabor kijelölő:az alábbi routek kivétele a táborkijelölés kényszeríése alól: gitPull, admin.index, logout (igen, nem lehetett kilépni tábor kijelölés nélkül xD)

--- app/Http/Middleware/TaborKijeloles.php
+++ app/Http/Middleware/TaborKijeloles.php
@@ -7,14 +7,25 @@ use App\Repositories\TaborRepository;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use ReflectionException;
 
 class TaborKijeloles
 {
     /**
+     * Azon route nevek listája, ahol az ellenőrzést nem kell végrehajtani
+     * @var array|string[]
+     */
+    protected array $kiveve = [
+        'admin.index',
+        'admin.gitPull',
+        'logout',
+    ];
+
+    /**
      * @param Request $request
      * @param Closure $next
      * @return mixed
-     * @throws \ReflectionException
+     * @throws ReflectionException
      * @throws NincsTaborMeghatarozvaException
      */
     public function handle(Request $request, Closure $next)
@@ -23,7 +34,13 @@ class TaborKijeloles
         if ($id = (int)$request->get('tabor_id')) {
             $taborRepo->setKijeloltTaborId($id);
         }
-        if (Auth::check() && empty($taborRepo->getKijeloltTaborId()) && ($request->route()->getName() !== 'admin.index')) {
+        if (
+            Auth::check()
+            &&
+            empty($taborRepo->getKijeloltTaborId())
+            &&
+            (!in_array($request->route()->getName(), $this->kiveve, true))
+        ) {
             if ($request->wantsJson()) {
                 throw new NincsTaborMeghatarozvaException();
             }
